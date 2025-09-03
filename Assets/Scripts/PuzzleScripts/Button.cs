afe8308445a4d818e5d7218722f6a80d88823c45
canaan2000 <canaanhutton@gmail.com>
2025-09-03 11:46:10 -0500

    InteractorMain using InputSystem

---
+++ Assets/Scripts/PuzzleScripts/Button.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool activeState = false;
+
+    public void Update()
+    {
+        if (activeState)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
--- Assets/Scripts/PuzzleScripts/Puzzle3.3/Button.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Button : MonoBehaviour
-{
-    public bool activeState = false;
-}
