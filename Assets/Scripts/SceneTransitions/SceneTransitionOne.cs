1fb2cd60e32174213e65ff397923c790dd65e861
Walrus-Dog <walrusdog@gmail.com>
2025-09-10 11:59:49 -0500

    Skybox changes
    Tweaks to the skyboxes

--- Assets/Scripts/SceneTransitions/SceneTransitionOne.cs
+++ Assets/Scripts/SceneTransitions/SceneTransitionOne.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+
+    public float delayTime = 2.0f;
+
+    public void LoadNextScene()
+    {
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && other.TryGetComponent<Player>(out var player))
