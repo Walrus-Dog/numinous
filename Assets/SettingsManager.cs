cc8631ce582b29ada4d980f4f5a018bc15409b87
Walrus-Dog <walrusdog@gmail.com>
2025-10-13 11:00:38 -0500

    Train Station Update
    Added new train station
    Working on lighting fixes

--- Assets/SettingsManager.cs
+++ Assets/SettingsManager.cs
@@ -8,22 +8,8 @@ public class SettingsManager : MonoBehaviour
 {
     public TMP_Dropdown graphicsDropdown;
 
-
-
-
     public void ChangeGraphicsQuality()
     {
         QualitySettings.SetQualityLevel(graphicsDropdown.value);
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
